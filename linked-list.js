507bff30901ce157c34bc4954c26714b7b288133
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-13 12:14:18 +0100

    add append method for linkedList class

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,4 @@
 class Node {
-
   constructor(value = null) {
     this.value = value;
     this.nextNode = null;
@@ -7,16 +6,30 @@ class Node {
 }
 
 class LinkedList {
-
   constructor() {
     this.head = null;
     this.tail = null;
     this.size = 0;
   }
+
+  append(value) {
+    if (this.tail === null && this.head === null) {
+      this.tail = new Node(value);
+      this.head = new Node(value);
+    } else if (this.head !== null && this.tail !== null) {
+      const temp = this.head;
+      this.head  = new Node(value);
+      this.head.next = temp;
+    }
+
+    this.size += 1;
+  }
 }
 
 const linkedList = new LinkedList();
 console.log(linkedList);
-const linkedList2 = new LinkedList();
-console.log(linkedList == linkedList2)
-console.log(linkedList === linkedList2)
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log(linkedList);
