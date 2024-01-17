3a3a0257b40f32347340f0c09fd65ac13a6d2162
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-17 04:58:09 +0100

    remove code testing and export full linkedList class

--- linked-list.js
+++ linked-list.js
@@ -1,6 +1,6 @@
 const Node = require('./node.js');
 
-class LinkedList {
+module.exports = class LinkedList {
   #size = 0;
   #head = null;
 
@@ -187,28 +187,4 @@ class LinkedList {
 
     return (output += 'null');
   }
-}
-
-const linkedList = new LinkedList();
-
-linkedList.prepend('test1');
-linkedList.append('test2');
-linkedList.append('test3');
-console.log(linkedList.toString()); // (test1) -> (test2) -> (test3) -> null
-console.log(linkedList.size); // 3
-console.log(linkedList.head); // return head Node
-console.log(linkedList.tail); // Node { value: 'test3', nextNode: null }
-console.log(linkedList.at(2)); // Node { value: 'test3', nextNode: null }
-console.log(linkedList.at(4)); // There is no item for this index
-linkedList.pop();
-console.log(linkedList.toString()); // (test1) -> (test2) -> null
-console.log(linkedList.contains('test1')); // true
-console.log(linkedList.find('test2')); // 1
-linkedList.prepend('test3');
-console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> null
-linkedList.insertAt('test4', 2);
-console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> null
-linkedList.insertAt('test5', 8);
-console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> (test5) -> null
-linkedList.removeAt(0);
-console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> (test5) -> null
+};
